1f627ca12b175fc51932bfca51747a1473673a2c
Siarhei Bokuts <siarheibokuts@gmail.com>
2025-10-10 00:57:53 +0200

    added GraphQL

--- index.ts
+++ index.ts
@@ -1,11 +1,39 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { kafkaService, USER_EVENTS_TOPIC } from "./src/kafka";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import http from "http";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const httpServer = http.createServer(app);
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
 
 // JSON middleware для body
 app.use(express.json());
@@ -35,13 +63,17 @@ app.listen(port, async () => {
     USER_EVENTS_TOPIC,
     async ({ topic, partition, value }) => {
       console.log(
-        `consumer works on Topic: ${topic} | Partition: ${partition} | Message: ${value}`,
+        `consumer works on Topic: ${topic} | Partition: ${partition} | Message: ${value}`
       );
 
       const event = JSON.parse(value);
       console.log("Received event:", event);
-    },
+    }
   );
+
+  await server.start();
+
+  app.use(cors(), express.json(), expressMiddleware(server));
 
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
